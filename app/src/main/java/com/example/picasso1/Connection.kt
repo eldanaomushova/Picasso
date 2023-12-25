4f8cb793f8b82a32ed73c776316d655c007e1e66
EldanaQuuene <eldana.omushova@alatoo.edu.kg>
2023-12-25 13:39:30 +0600

    2cpm

---
+++ app/src/main/java/com/example/picasso1/Connection.kt
@@ -0,0 +1,186 @@
+package com.example.picasso1
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.app.ActivityCompat
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.util.UUID
+class Connection : AppCompatActivity() {
+    private lateinit var connectionbtn: Button
+    private lateinit var getDevisebtn: Button
+    private lateinit var sendbutton: Button
+    private lateinit var text1: TextView
+    private lateinit var result: TextView
+    private lateinit var bluetoothAdaptor: BluetoothAdapter
+    private lateinit var bluetoothDevice: BluetoothDevice
+    private lateinit var bluetoothSocket: BluetoothSocket
+    private lateinit var intentFilter: IntentFilter
+    private lateinit var inputStream: InputStream;
+    private lateinit var outputStream: OutputStream;
+    private lateinit var rxThread: RxThread;
+    private var rxData: String = "";
+    private val REQUEST_BLUETOOTH_PERMISSION = 1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_connection)
+        if (!hasBluetoothPermissions()) {
+            requestBluetoothPermissions()
+        }
+        connectionbtn = findViewById(R.id.connbtn)
+        getDevisebtn = findViewById(R.id.getDevise)
+        sendbutton = findViewById(R.id.sendbtn)
+        text1 = findViewById(R.id.txt1)
+        result = findViewById(R.id.txt2)
+        bluetoothAdaptor = BluetoothAdapter.getDefaultAdapter()
+        intentFilter = IntentFilter()
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
+        intentFilter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED)
+        registerReceiver(Btreceiver, intentFilter)
+        connectionbtn.isEnabled = false;
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+
+        getDevisebtn.setOnClickListener(View.OnClickListener {
+            val devices: Set<BluetoothDevice> = bluetoothAdaptor.bondedDevices
+            var isHC05Found = false
+
+            for (dev in devices) {
+                if (dev.name == "HC-05") {
+                    bluetoothDevice = dev
+                    isHC05Found = true
+                    break
+                }
+            }
+            if (isHC05Found) {
+                Toast.makeText(applicationContext, "Device found: HC-05", Toast.LENGTH_SHORT).show()
+                connectionbtn.isEnabled = true
+            } else {
+                Toast.makeText(applicationContext, "HC-05 not found among bonded devices", Toast.LENGTH_SHORT).show()
+            }
+        })
+        connectionbtn.setOnClickListener(View.OnClickListener {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.BLUETOOTH
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return@OnClickListener
+            }
+            try {
+                bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"))
+                bluetoothSocket.connect()
+                inputStream = bluetoothSocket.inputStream
+                outputStream = bluetoothSocket.outputStream
+                rxThread = RxThread(inputStream)
+                rxThread.isRunning = true
+                rxThread.start()
+                if(rxThread.isRunning){
+                    Toast.makeText(applicationContext, "Connected", Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(applicationContext, "Error with connecting", Toast.LENGTH_SHORT).show()
+                }
+
+            } catch (e: IOException) {
+                e.printStackTrace()
+                Toast.makeText(applicationContext, "Error with connecting", Toast.LENGTH_SHORT).show()
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(applicationContext, "Error with connecting", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+
+        sendbutton.setOnClickListener(View.OnClickListener {
+            try {
+                outputStream.write((text1.text.toString() + "\r\n").toByteArray())
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        })
+    }
+    private fun hasBluetoothPermissions(): Boolean {
+        return (checkSelfPermission(Manifest.permission.BLUETOOTH) == PackageManager.PERMISSION_GRANTED &&
+                checkSelfPermission(Manifest.permission.BLUETOOTH_ADMIN) == PackageManager.PERMISSION_GRANTED)
+    }
+    private fun requestBluetoothPermissions() {
+        requestPermissions(
+            arrayOf(
+                Manifest.permission.BLUETOOTH,
+                Manifest.permission.BLUETOOTH_ADMIN
+            ),
+            REQUEST_BLUETOOTH_PERMISSION
+        )
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_BLUETOOTH_PERMISSION -> {
+                if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+
+                } else {
+
+                }
+            }
+        }
+    }
+    class RxThread(private val inputStream: InputStream) : Thread() {
+        var isRunning: Boolean = false
+        init {
+            isRunning = false
+        }
+        override fun run() {
+            while (isRunning) {
+                try {
+                    if (inputStream.available() > 0) {
+                        // Read data from the inputStream and process it
+                        val data = ByteArray(inputStream.available())
+                        inputStream.read(data)
+                        val receivedData = String(data)
+                        // Process receivedData as needed
+                        println("Received data: $receivedData")
+                    }
+                    Thread.sleep(10)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                    // Handle IOException
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                    // Handle InterruptedException
+                }
+            }
+        }
+    }
+    private var Btreceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val action = intent.action
+            if (action != null) {
+                when (action) {
+                    BluetoothAdapter.ACTION_DISCOVERY_FINISHED -> {
+                        connectionbtn.isEnabled = true
+                    }
+                    BluetoothDevice.ACTION_ACL_DISCONNECTED -> {
+                        rxThread.isRunning = false;
+                    }
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/picasso1/MainActivity.kt
+++ app/src/main/java/com/example/picasso1/MainActivity.kt
@@ -1,35 +1,39 @@
 package com.example.picasso1
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-
 class MainActivity : AppCompatActivity() {
     private lateinit var button: Button
+    private lateinit var button2: Button
     private lateinit var imageView: ImageView
-
     companion object {
         val IMAGE_REQUEST_CODE = 100
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button = findViewById(R.id.btn_pick_img)
+        button2 = findViewById(R.id.gotosec)
         imageView = findViewById(R.id.img)
         button.setOnClickListener {
             pickImageGallery()
         }
+        button2.setOnClickListener{
+            try {
+                val intent = Intent(this@MainActivity, Connection::class.java)
+                startActivity(intent)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
-
     private fun pickImageGallery() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
         startActivityForResult(intent, IMAGE_REQUEST_CODE)
     }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK) {
